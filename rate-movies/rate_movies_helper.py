16fca8d373dc8b1e06a621be635f36d451322d7b
Thomas Breydo <tbreydo@gmail.com>
2020-07-11 17:11:40 -0400

    begin transferring rate-movies

---
+++ rate-movies/rate_movies_helper.py
@@ -0,0 +1,8 @@
+'''YOU ARE IN THE WRONG PLACE. THIS FILE EXISTS JUST TO PROVIDE CERTAIN
+FUNCTIONS TO rate_movies.ipynb'''
+
+import sys
+import os
+sys.path.insert(0, os.path.join(
+    os.path.dirname(__file__), '..', '..', 'dataset'))
+import movielens  # noqa
