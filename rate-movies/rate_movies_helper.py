b275cf192e3dea1d0712e5b5ecee0efc9713ee69
Thomas Breydo <tbreydo@gmail.com>
2020-07-12 10:44:34 -0400

    first rate-movies up and running!

--- rate-movies/rate_movies_helper.py
+++
@@ -1,9 +0,0 @@
-'''YOU ARE IN THE WRONG PLACE. THIS FILE EXISTS JUST TO PROVIDE 
-rate_movies.ipynb SOME OTHERWISE INACCESSIBLE FUNCTIONS.'''
-
-import sys
-import os
-sys.path.insert(0, os.path.join(
-    os.path.dirname(__file__), '..', '..', 'movie-recommender-2'))
-import dataset  # noqa
-import posters  # noqa
